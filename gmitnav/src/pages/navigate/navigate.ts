0a2e9e257569d18a4bc4af98d4840b9fcc9a7328
repickym <repickym@gmail.com>
2017-04-21 12:54:12 +0100

    Tabs update and Logo added

--- gmitnav/src/pages/navigate/navigate.ts
+++ gmitnav/src/pages/navigate/navigate.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'navigate',
+  templateUrl: 'navigate.html'
 })
-export class ContactPage {
+export class NavigatePage {
 
   constructor(public navCtrl: NavController) {
 
--- gmitnav/src/pages/tabs/tabs.ts
+++ gmitnav/src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { MapPage } from '../map/map';
+import { NavigatePage } from '../navigate/navigate';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root: any = AboutPage;
+  tab2Root: any = MapPage;
+  tab3Root: any = NavigatePage;
 
   constructor() {
 
